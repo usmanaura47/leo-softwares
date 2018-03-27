2e9f7916afced95d8e2f3ea53b02ae46a8159295
usman47 <usmanaura47@gmail.com>
2018-03-27 16:56:29 +0500

    Updated

---
+++ js/custom.js
@@ -0,0 +1,43 @@
+
+$(window).load(function(){
+	$(".pageloader").fadeOut(300);
+});
+	
+$(document).ready(function() {
+		
+	$('#fullpage').fullpage({
+		anchors: ['home', 'whoarewe', 'projects', 'team', 'contact'],
+		navigation: true,
+		bigSectionsDestination: top,
+		navigationPosition: 'right',
+		navigationTooltips: ['Home', 'Who are we?', 'Our Projects', 'Our Team', 'Contact Us'],
+		autoScrolling: false,
+		fitToSection: false,
+		keyboardScrolling: true,
+		animateAnchor: true,
+		lockAnchors: true,
+		touchSensitivity: 15
+	});
+			
+	$(document).ready(function() {  
+		$("html").niceScroll({
+			cursorwidth: "8px",
+			cursorminheight: "140",
+			horizrailenabled: false
+		});
+		$(".pageloader").niceScroll({
+			cursorwidth: "8px",
+			cursorminheight: "140",
+			horizrailenabled: false
+		});
+	});
+			
+	$('#downwego').click(function(){
+		$.fn.fullpage.moveSectionDown(); 
+	});
+			
+	$('.response').click(function(){
+		$(this).hide(1200);
+	});
+			
+});
